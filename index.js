d4f473b894b0222311af949440b83f93f84044bf
Michel Marinho <marinhomich@gmail.com>
2024-04-25 10:32:37 -0400

    writeFile works!

--- index.js
+++ index.js
@@ -1,47 +1,47 @@
-import { readFileSync } from "fs";
+import { readFileSync, writeFile } from "fs";
 import { load } from "js-yaml";
 
-function verifyYAMLFile(filePath) {
-  try {
-    // Read the YAML file
-    const yamlContent = readFileSync(filePath, "utf8");
+const locale1 = "en-US";
+const locale2 = "pt-BR";
+const fileName = "state_of_html_2023.yml";
+const fileNameWithoutExtension = fileName.replace(/\.yml$/, "");
 
-    // Parse the YAML content
-    const data = load(yamlContent);
-    const formatedData = data.translations.map(({ key }) => key);
-    return formatedData;
-  } catch (error) {
-    // If an error occurs during parsing, return false and log the error
-    console.error("Error parsing YAML file:", error.message);
-    return false;
+function verifyYAMLFile(fileName) {
+  const results = [];
+  let locales = [`./docs/${locale1}`, `./docs/${locale2}`];
+
+  for (const locale of locales) {
+    try {
+      const yamlContent = readFileSync(`${locale}/${fileName}`, "utf8");
+      const data = load(yamlContent);
+      const formatedData = data.translations.map(({ key }) => key);
+
+      results.push(formatedData);
+    } catch (error) {
+      console.error("Error parsing YAML file:", error.message);
+      results.push(false);
+    }
   }
+
+  return results;
 }
 
-function verifyYAMLFile2(filePath) {
-  try {
-    // Read the YAML file
-    const yamlContent = readFileSync(filePath, "utf8");
+function findItemsNotIncluded() {
+  const locale1 = verifyYAMLFile(fileName)[0];
+  const locale2 = verifyYAMLFile(fileName)[1];
+  return locale1.filter((item) => !locale2.includes(item));
+}
+const itemsNotIncluded = findItemsNotIncluded();
 
-    // Parse the YAML content
-    const data = load(yamlContent);
-    const formatedData = data.translations.map(({ key }) => key);
-    return formatedData;
-  } catch (error) {
-    // If an error occurs during parsing, return false and log the error
-    console.error("Error parsing YAML file:", error.message);
-    return false;
+const resultString =
+  `key not included in ${locale2}:\n\n` + itemsNotIncluded.join("\n");
+
+// Write the result to a file named output.txt
+writeFile(
+  `./docs/${locale2}/missing-keys-${fileNameWithoutExtension}.md`,
+  resultString,
+  (err) => {
+    if (err) throw err;
+    console.log("Result has been saved to output.txt");
   }
-}
-
-// Example usage
-const filePath = "./docs/en-US/state_of_html_2023.yml"; // Change this to the path of your YAML file
-const filePath2 = "./docs/pt-BR/state_of_html_2023.yml"; // Change this to the path of your YAML file
-
-function findItemsNotIncluded(arrayA, arrayB) {
-  return arrayA.filter((item) => !arrayB.includes(item));
-}
-
-const isYAMLValid = verifyYAMLFile(filePath);
-const isYAMLValid2 = verifyYAMLFile2(filePath2);
-
-console.log(findItemsNotIncluded(isYAMLValid, isYAMLValid2));
+);
